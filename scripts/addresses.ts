7d3fa3f9acf9afd723d3abbe06f0ff6cc7377951
RiccardoGalbusera <93911514+RiccardoGalbusera@users.noreply.github.com>
2023-02-15 11:23:20 +0100

    clean: remove unused scripts and rename files

---
+++ scripts/addresses.ts
@@ -0,0 +1,9 @@
+export const TIDE_VERIFIER = "0x75d14f0ae59003c0806b625b402a40340ffde634";
+export const TIDE_KEEPER = "0x607291c9b3b03d8c2dc1f5f7f8db2b6a06c91183";
+
+export const TRUSTED_FORWARDERS = {
+  1: "",
+  10: "",
+  137: "",
+  42161: "",
+};
--- scripts/deployFirstCampaign.ts
+++ scripts/deployFirstCampaign.ts
@@ -1,29 +1,36 @@
 import { ethers } from "hardhat";
 
-import { WaveContract, WaveFactory } from "../typechain-types/contracts/claim-with-permit";
+import { WaveFactory } from "../typechain-types/contracts/core";
+import { TIDE_KEEPER, TIDE_VERIFIER, TRUSTED_FORWARDERS } from "./addresses";
 
 async function main() {
   console.log("Deploying the factory and a test campaign together...");
   const signer = (await ethers.getSigners())[0];
 
   const WaveFactoryFactory = await ethers.getContractFactory(
-    "contracts/claim-with-permit/WaveFactory.sol:WaveFactory",
+    "contracts/core/WaveFactory.sol:WaveFactory",
     signer
   );
 
+  // @dev Fill the following data before deploying
   // Factory into
-  const keeper = "0x607291c9b3b03d8c2dc1f5f7f8db2b6a06c91183";
-  const trustedForwarderOptimism = "0x61ea47C9f2D33Eb8283e48776794f30A52c92A0B";
-  const verifier = "0x75d14f0ae59003c0806b625b402a40340ffde634";
+  const chainId = 42161;
+  const keeper = TIDE_KEEPER;
+  const trustedForwarder = TRUSTED_FORWARDERS[chainId];
+  const verifier = TIDE_VERIFIER;
 
   // Wave info
-  const name = "Test Campaign";
-  const symbol = "TEST";
-  const baseURI = "test";
+  const name = "Campaign Name";
+  const symbol = "SYM";
+  const baseURI = "";
   const beginTimestamp = 1561534220;
   const endTimestamp = 2771936220;
 
-  const WaveFactoryTx = await WaveFactoryFactory.deploy(keeper, trustedForwarderOptimism, verifier);
+  const WaveFactoryTx = await WaveFactoryFactory.deploy(
+    keeper,
+    trustedForwarder,
+    verifier
+  );
   const factoryAddress = await WaveFactoryTx.deployed().then((x) => x.address);
   console.log("factory deployed at: ", factoryAddress);
   await new Promise((resolve) => setTimeout(resolve, 5000));
