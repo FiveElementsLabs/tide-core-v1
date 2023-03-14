7c0472c597e4edd22c97f35893c7fa0b881d32a8
Nicolas Racchi <nicolas@racchi.com>
2023-03-14 20:33:02 +0100

    chore: updated forwarders

--- scripts/deployForwarder.ts
+++ scripts/deployForwarder.ts
@@ -4,10 +4,7 @@ async function main() {
   console.log("Deploying forwarder...");
   const signer = (await ethers.getSigners())[0];
 
-  const ForwarderFactory = await ethers.getContractFactory(
-    "MinimalForwarder",
-    signer
-  );
+  const ForwarderFactory = await ethers.getContractFactory("Forwarder", signer);
 
   const ForwarderTx = await ForwarderFactory.deploy();
   const forwarderAddress = await ForwarderTx.deployed().then((x) => x.address);
