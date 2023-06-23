460dbb13e0bfca11890aa0d7c770791045b8cec0
nicolas <48695862+merklefruit@users.noreply.github.com>
2023-06-23 16:32:32 +0200

    chore: slight modifications to allow for mainnet factory

---
+++ arguments.js
@@ -0,0 +1,6 @@
+// Constructor arguments used to verify the WaveFactory on Etherscan
+module.exports = [
+  "0x607291c9b3b03d8c2dc1f5f7f8db2b6a06c91183",
+  "0xdead00000000000000000000000000000000dead",
+  "0x75d14f0ae59003c0806b625b402a40340ffde634",
+];
